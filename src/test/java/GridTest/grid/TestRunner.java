ddf4a4da42fb695858bf4d54ae155366c43e5212
Max Karpov <bsativa@live.com>
2018-02-06 20:39:24 +0000

    test message5

--- src/test/java/GridTest/grid/TestRunner.java
+++ src/test/java/GridTest/grid/TestRunner.java
@@ -8,6 +8,6 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features= {"src/test/test.feature"},
 plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/report.html"})
-public class Runner {
+public class TestRunner {
 
 }
